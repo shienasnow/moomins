098fa6051016e591509c0395d47f91076856f610
ParkHashhh <qkrwntjr1255@naver.com>
2024-11-24 18:03:59 +0900

    asset_loader.py modify 11/24 js

--- core/asset_maya/scripts/loader/asset_loader.py
+++ core/asset_maya/scripts/loader/asset_loader.py
@@ -17,7 +17,6 @@ from PySide6.QtCore import QFile,QEvent,QPoint
 from PySide6.QtCore import Qt,QUrl
 from PySide6.QtGui import QDesktopServices,QPixmap,QGuiApplication
 from datetime import datetime
-sys.path.append("/home/rapa/git/pipeline/api_scripts")
 
 from shotgun_api import ShotgunApi
 
